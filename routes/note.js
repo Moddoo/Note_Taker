f8a4733e18ef4636df874a8f99a0818113f74d3f
Moddoo <zoddoo@yahoo.com>
2020-03-23 01:03:12 -0500

    ms ALLAH

--- routes/note.js
+++ routes/note.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 let savedNotes = [];
-let id = 1;
+let x = 1;
 
 router
    .route('/')
@@ -11,34 +11,33 @@ router
        fs.readFile(`${__dirname}/db.json`, (err,data) => {
            if(err) throw err;
            savedNotes = JSON.parse(data);
-       });
-       res.status(200).sendFile(`${__dirname}/db.json`);
+           res.status(200).json(savedNotes)
     })
+})
+   
    .post((req,res) => {
-   let newNote = Object.assign({id: id}, req.body)
-       savedNotes.push(newNote)
+    id = Array.from({length: savedNotes.length}, (k,v) => ++v).filter(el => savedNotes.map(e => e.id).indexOf(el) === -1)[0];
+    if(!id)  {
+        id = x;
+        x++;
+    }else x = savedNotes.length + 2;
+   let newNote = Object.assign({id: id}, req.body);
+       savedNotes.push(newNote);
    fs.writeFile(`${__dirname}/db.json`,JSON.stringify(savedNotes, null, 2) , err => {
        if(err) throw err;
    })
-   res.status(201).json(savedNotes)
-   return id++;
+   res.status(201).json(savedNotes);
 });
 
 router.delete('/:id', (req,res) => {
     let noteId = req.params.id;
-    if(isNaN(noteId) || !noteId)
+    if(isNaN(noteId) || !noteId || noteId < 1)
     return res.status(404).json({
         status: 'Fail',
         message: 'Invalid ID'
     });
     let note = savedNotes.findIndex(el => el.id == noteId);
     savedNotes.splice(note,1);
-    id = 1;
-    savedNotes.map(el =>{
-        el.id = id ;
-        id++;
-        return el;
-    } )
     fs.writeFile(`${__dirname}/db.json`, JSON.stringify(savedNotes, null, 2) , err => {
         if(err) throw err;
         res.sendFile(`${__dirname}/db.json`)
